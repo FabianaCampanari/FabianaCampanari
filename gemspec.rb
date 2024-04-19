f033da72c00b5125a11d14510fcdd7912bab62d7
Fabiana Campanari <fabicampanari@gmail.com>
2024-04-19 20:26:26 -0300

    gemspec.rb
    Signed-off-by: Fabiana Campanari <fabicampanari@gmail.com>

---
+++ gemspec.rb
@@ -0,0 +1,20 @@
+ bundle gem my_gem
+Creating gem 'my_gem'...
+      create  my_gem/Gemfile
+      create  my_gem/.gitignore
+      create  my_gem/lib/my_gem.rb
+      create  my_gem/lib/my_gem/version.rb
+      create  my_gem/my_gem.gemspec
+      create  my_gem/Rakefile
+      create  my_gem/README.md
+      create  my_gem/bin/console
+      create  my_gem/bin/setup
+      create  my_gem/CODE_OF_CONDUCT.md
+      create  my_gem/LICENSE.txt
+      create  my_gem/.travis.yml
+      create  my_gem/test/test_helper.rb
+      create  my_gem/test/my_gem_test.rb
+Initializing git repo in ./my_gem
+
+
+
